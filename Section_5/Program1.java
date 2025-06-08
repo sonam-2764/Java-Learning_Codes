df5f928963b744b79078fdb5500d138a42ba2ae8
Sonam Kumari <aspritualgirl@gmail.com>
2025-06-08 10:07:40 +0530

    comitted

--- Section_5/Program1.java
+++ Section_5/Program1.java
@@ -4,5 +4,6 @@ public class Program1 {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("sonam is a good girl");
+        System.out.println("gadhi");
     }
 }

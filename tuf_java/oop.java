7ab3eb9a8b40b916520ec3b51cf9cb24f6c1984d
Sonam Kumari <aspritualgirl@gmail.com>
2026-09-08 16:47:50 +0530

    object and class

---
+++ tuf_java/oop.java
@@ -0,0 +1,32 @@
+package tuf_java;
+// class Test{
+//     public static void printTest(){
+//         System.out.println("hello");
+//     }
+// }
+
+// public class oop {
+//     public static void main(String[] args) {
+//         Test.printTest();//we can access static method in main without creating object
+//     }
+// }
+
+class Test{
+    int age;
+    public void assignAge(int num){
+        age = num;
+    }
+}
+
+class oop {
+    public static void main(String[] args) {
+        Test test1 = new Test();
+        test1.assignAge(num:10);
+
+        Test test2 = new Test();
+        test2.assignAge(19);
+
+        System.out.println(test1.age);
+        System.out.println(test2.age);
+    }
+}
